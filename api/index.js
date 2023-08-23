d6dfb408eee59521bf277fcc8989042ae448497c
Pablo Gumilla <pablo.gumilla@gmail.com>
2023-08-23 18:03:30 -0300

    Add USD currency endpoint

--- api/index.js
+++ api/index.js
@@ -1,16 +1,10 @@
 const app = require('express')();
-const { v4 } = require('uuid');
+const fetch = require('node-fetch')
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+app.post('/api/currency/usd', async (req, res) => {
 
-app.post('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+  const result = await fetch('https://dolarapi.com/v1/dolares')
+  res.end(`Currency: ${result}`);
 });
 
 module.exports = app;
